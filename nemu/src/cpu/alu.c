1a64ed70a6d356956bf1898d19e455bce5ed4c56
monitor <tracer@njuics.org>
2019-09-15 19:33:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -63,7 +63,15 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
-
+uint32_t set_CF_adc(uint32_t result, uint32_t src, size_t data_size)
+{
+	reult = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	src = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	if(cpu.eflags.CF == 0)
+		cpu.eflags.CF = result < src;
+	else
+		cpu.eflags.CF = result <= src;
+}
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
@@ -71,7 +79,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	uint32_t res = 0;
 	res = dest + src + cpu.eflags.CF;
-	set_CF_add(res, src, data_size);
+	set_CF_adc(res, src, data_size);
 	set_OF_add(res, src, dest, data_size);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
@@ -87,7 +95,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = 0;
 	uint32_t src_tra = src ^ (0xFFFFFFFF >> (32 - data_size));
 	res = dest + src_tra + 0x1;
-	set_CF_add(res, src_tra, data_size);
+	set_CF_adc(res, src_tra, data_size);
 	set_OF_add(res, src_tra, dest, data_size);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
