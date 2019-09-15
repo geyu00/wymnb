7d9eaa042aea43dec6e90a9168a6184db6d9fa1c
tracer-pa-public <tracer@njuics.org>
2019-09-15 18:57:10 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  18:57:10 up  2:30,  1 user,  load average: 0.00, 0.00, 0.00 1ce0485b90c6e6dc1de1b58414aa4d50f0c5cf

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -59,6 +59,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
 	set_PF(res);
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
