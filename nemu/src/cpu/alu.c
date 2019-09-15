67432bc74640ad26e10d46318fae80b56f1daac3
tracer-pa-public <tracer@njuics.org>
2019-09-15 20:21:23 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  20:21:23 up  3:54,  1 user,  load average: 0.10, 0.03, 0.01 759cbbf7b3ac290efea413416f7e9ea288665d82

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -115,7 +115,6 @@ void set_CF_sbb(uint32_t dest, uint32_t src, size_t data_size)
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	cpu.eflags.CF = dest < (src + cpu.eflags.CF);
 }
-void set_OF_sbb(uint32_t result,
 uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
