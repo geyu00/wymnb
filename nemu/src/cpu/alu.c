99b0500b2db8e68bc82d549a8321f8c194d69786
tracer-pa-public <tracer@njuics.org>
2019-09-16 10:29:22 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  10:29:22 up 18:02,  1 user,  load average: 0.00, 0.00, 0.00 1fffb57542fa5b7637926b8fad7a47beda2bfe40

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -139,9 +139,11 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_mul(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	src &= (0xFFFFFFFF >> (32 - data_size));
+	dest &= (0xFFFFFFFF >> (32 - data_size));
+	uint64_t res = 0;
+	res = (uint64_t)src * (uint64_t)dest;
+	return res & (0xFFFFFFFFFFFFFFFF >> (64 - 2 * data_size));
 #endif
 }
 
@@ -154,7 +156,7 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 	int64_t dest_64 = sign_ext_64(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	int64_t res = 0;
 	res = src_64 * dest_64;
-	return res & (0xFFFFFFFFFFFFFFFF >> (64 - 2 * data_size));
+	return res;
 #endif
 }
 
