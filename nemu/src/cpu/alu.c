e745be8f184492ed0beaa2d810ad8308dd6f5a01
tracer-pa-public <tracer@njuics.org>
2019-09-16 10:54:47 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  10:54:47 up 18:28,  1 user,  load average: 0.00, 0.02, 0.00 2de2415f686c16134965acad7a6257b879381a4f

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -198,9 +198,12 @@ uint32_t alu_mod(uint64_t src, uint64_t dest)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_mod(src, dest);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	src &= (0xFFFFFFFFFFFFFFFF >> (64 - data_size));
+	dest &= (0xFFFFFFFFFFFFFFFF >> (64 - data_size));
+	uint32_t res = 0;
+	if(src == 0) return -1;
+	res = dest % src;
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
