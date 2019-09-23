1e2951dd3d1542f603e01d0b9baff7a0a657a4a4
tracer-pa-public <tracer@njuics.org>
2019-09-23 23:00:30 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:00:30 up  1:39,  1 user,  load average: 0.02, 0.03, 0.00 708544a95d977f374ddcd9d39834bd40b85e9efb

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -87,11 +87,11 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
-void set_CF_sub(uint32_t dest, uint32_t src, size_t data_size)
+void set_CF_sub(uint32_t dest, uint32_t res, size_t data_size)
 {
 	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
-	cpu.eflags.CF = dest < src;
+	cpu.eflags.CF = dest < res;
 }
 uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 {
@@ -101,7 +101,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = 0;
 	uint32_t src_tra = src ^ (0xFFFFFFFF >> (32 - data_size));
 	res = dest + src_tra + 0x1;
-	set_CF_sub(dest, src, data_size);
+	set_CF_sub(dest, res, data_size);
 	set_OF_add(res, src_tra, dest, data_size);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
