a095e51a33fa8905e980f7ab59229756f058934e
monitor <tracer@njuics.org>
2019-10-11 21:15:02 +0800

    git monitor commit.

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -1,4 +1,11 @@
-#ifndef __INSTR_MOV_H__
-#define __INSTR_MOV_H__
+#ifndef __INSTR_ADD_H__
+#define __INSTR_ADD_H__
+
+make_instr_func(add_r2rm_b);
+make_instr_func(add_r2rm_v);
+make_instr_func(add_rm2r_b);
+make_instr_func(add_rm2r_v);
+make_instr_func(add_i2a_b);
+make_instr_func(add_i2a_v);
 
 #endif
