6a9a412e7d73cdeee7bf7ea4375de7501331f570
monitor <tracer@njuics.org>
2019-10-11 22:14:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -3,7 +3,8 @@
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
-	opr_dest.val &= opr_src.val;
+	operand_read(&opr_dest);
+	opr_dest.val = alu_and(opr_src.val, opr_dest.val, opr_dest.data_size);
 	operand_write(&opr_dest);
 }
 
