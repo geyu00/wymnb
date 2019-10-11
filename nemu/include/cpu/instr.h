afec4b3af493d199ed3dfaa2e1bc54c7dc2026ae
tracer-pa-public <tracer@njuics.org>
2019-10-11 21:40:24 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:40:24 up  3:34,  1 user,  load average: 0.15, 0.13, 0.08 a7b474544b452802eb787be22a98c25c90ca38ce

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -14,6 +14,14 @@
 
 extern uint8_t data_size;
 
+#include "cpu/instr/cmp.h"
+#include "cpu/instr/xor.h"
+#include "cpu/instr/sub.h"
+#include "cpu/instr/and.h"
+#include "cpu/instr/sbb.h"
+#include "cpu/instr/adc.h"
+#include "cpu/instr/or.h"
+#include "cpu/instr/add.h"
 #include "cpu/instr/push.h"
 #include "cpu/instr/mov.h"
 #include "cpu/instr/jmp.h"
