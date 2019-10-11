53f74b905ba5231ea123a1bd1115bb7265df7115
tracer-pa-public <tracer@njuics.org>
2019-10-11 21:38:24 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:38:24 up  3:32,  1 user,  load average: 0.08, 0.07, 0.05 d6f5b4876a215d557569fca55dc26dd0b8749ccc

---
+++ nemu/src/cpu/instr/cmp.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	alu_sub(opr_src, opr_dest, data_size);
+}
+
+make_instr_impl_2op(mov, r, rm, b)
+make_instr_impl_2op(mov, r, rm, v)
+make_instr_impl_2op(mov, rm, r, b)
+make_instr_impl_2op(mov, rm, r, v)
+make_instr_impl_2op(mov, i, a, b)
+make_instr_impl_2op(mov, i, a, v)
