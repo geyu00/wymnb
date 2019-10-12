ff08bfbc3252c3f6c9ad2ce1d53674b930084945
tracer-pa-public <tracer@njuics.org>
2019-10-12 11:28:28 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:28:28 up 17:22,  1 user,  load average: 0.00, 0.01, 0.01 ff2746cff92c2c39b6dbd97827220920e96d685a

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -2,22 +2,22 @@
 
 make_instr_func(ret_near)
 {
-        OPERAND rel;
-        //rel.type = ;
+        /*OPERAND rel;
+        rel.type = ;
         rel.sreg = SREG_CS;
         rel.data_size = data_size;
-        rel.addr = eip + 1;
+        rel.addr = eip + 1;*/
 
-        operand_read(&rel);
+        //operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
+        //int offset = sign_ext(rel.val, data_size);
         // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("ret", "", 1 + data_size / 8, &rel);
+        print_asm_0("ret", "", 1);
 
-        cpu.eip -= offset;
-	cpu.esp += data_size / 8;
+        //cpu.eip -= offset;
+	cpu.esp += 4;
 
-        return 1 + data_size / 8;
+        return 1 ;
 
 	
 }
