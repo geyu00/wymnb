7edbaf52de6cfcb19cb19a75ea7f035f2f7ced53
tracer-pa-public <tracer@njuics.org>
2019-10-14 17:23:11 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:23:11 up 2 days, 17:31,  1 user,  load average: 0.17, 0.20, 0.12 8a993552dc5dd1759aa97b0f466f9abd867b41f8

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -14,5 +14,26 @@ make_instr_impl_2op(xor, rm, r, b)
 make_instr_impl_2op(xor, rm, r, v)
 make_instr_impl_2op(xor, i, a, b)
 make_instr_impl_2op(xor, i, a, v)
-make_instr_impl_2op(xor, i, rm, bv)
+//make_instr_impl_2op(xor, i, rm, bv)
 make_instr_impl_2op(xor, i, rm, v)
+
+make_instr_func(xor_i2rm_bv)
+{
+	int len = 1;
+        OPERAND rm, i;
+	
+        i.data_size = 8;
+	rm.data_size = data_size;
+	len += modrm_rm(eip + 1, &rm);
+        i.type = OPR_IMM;
+	i.sreg = SREG_GS;
+	i.addr = eip + len;
+	//rm.type = OPR_MEM;
+	len += i.data_size / 8;
+        operand_read(&i);
+        operand_read(&rm);
+        rm.val = alu_xor(sign_ext(i.val, 8), rm.val, rm.data_size);
+        operand_write(&rm);
+	print_asm_2("xor", "", len, &i, &rm);
+        return len;
+}
