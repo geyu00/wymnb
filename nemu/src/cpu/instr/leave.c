1c96e0b32beb402d6a136b6b13ff0eb327db02ef
tracer-pa-public <tracer@njuics.org>
2019-10-15 00:03:39 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  00:03:39 up 2 days, 23:27,  1 user,  load average: 0.00, 0.04, 0.01 2e707d48e84de975f6150b085db23992b0ea0055

---
+++ nemu/src/cpu/instr/leave.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(leave)
+{
+	cpu.esp = cpu.ebp;
+	OPERAND rel;
+	rel.type = OPE_MEM;
+	rel.data_size = data_size;
+	rel.addr = cpu.esp;
+	operand_read(&rel);
+	cpu.ebp = rel.val;
+	cpu.esp += data_size / 8;
+	print_asm_0("leave", "",1);
+	return 1;
+}
