98a58270b9421a6a848d43360d628d81a0400950
monitor <tracer@njuics.org>
2019-10-14 22:56:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -8,9 +8,7 @@ static void instr_execute_1op()
 	operand_read(&rel);
 	opr_src.val = rel.val;
 	cpu.esp += opr_src.data_size / 8;
-	operand_write(&opr_src);
-	
-	
+	operand_write(&opr_src);	
 }
 make_instr_impl_1op(pop, r, v)
 make_instr_impl_1op(pop, rm, v)
