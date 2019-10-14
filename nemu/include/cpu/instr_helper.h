e295535e5db0e9d6950e35532f4da997a8ba6fb6
tracer-pa-public <tracer@njuics.org>
2019-10-14 11:03:28 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:03:28 up 2 days, 13:59,  1 user,  load average: 0.00, 0.04, 0.06 3ce2b551535b8b83cacd61d81b9502c2878705e

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -206,7 +206,7 @@ static inline bool inv_cc();
 	cpu.eflags.SF == 0
 
 #define condition_g \
-	(cpu.eflags.ZF == 0) && (cpu.eflags.SF == cpu.eflags.OF)
+	(cpu.eflags.ZF == 0) && (cpu.eflags.OF == cpu.eflags.SF)
 
 #define condition_ge \
 	cpu.eflags.SF == cpu.eflags.OF
