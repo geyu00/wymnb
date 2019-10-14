0aa7e2e0440723b2c3ec35f86d5e3dc63192267f
tracer-pa-public <tracer@njuics.org>
2019-10-14 15:13:16 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:13:16 up 2 days, 15:44,  1 user,  load average: 0.23, 0.19, 0.10 f9815a63bd3705f42ac4b35222d6fed2324554d9

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -1,11 +1,11 @@
 #include "cpu/instr.h"
 static void instr_execute_1op() 
 {
-	operand_read(&opr_src);
-	cpu.esp -= opr_src.data_size / 8;
+	//operand_read(&opr_src);
+	cpu.esp += opr_src.data_size / 8;
 	opr_src.addr = cpu.esp;	
-	opr_src.type = OPR_MEM;
-	operand_write(&opr_src);
+	//opr_src.type = OPR_MEM;
+	//operand_write(&opr_src);
 }
 make_instr_impl_1op(pop, r, v)
 make_instr_impl_1op(pop, rm, v)
