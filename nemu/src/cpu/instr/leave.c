857b2b5e7907340c988ff92473d32558762e228d
tracer-pa-public <tracer@njuics.org>
2019-10-15 09:11:12 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  09:11:12 up 3 days,  8:10,  1 user,  load average: 0.13, 0.04, 0.01 c42706d677d0e8bb828c22c7c4e068fc3ff000c5

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -8,7 +8,14 @@ make_instr_func(leave)
 	rel.data_size = data_size;
 	rel.addr = cpu.esp;
 	operand_read(&rel);
-	cpu.ebp = rel.val;
+
+	OPERAND ebp;
+	ebp.type = OPR_MEM;
+	ebp.data_size = data_size;
+	ebp.addr = REG_BP;
+	ebp.val = rel.val;
+	operand_read(&ebp);	
+	//cpu.ebp = rel.val;
 	cpu.esp += data_size / 8;
 	print_asm_0("leave", "",1);
 	return 1;
