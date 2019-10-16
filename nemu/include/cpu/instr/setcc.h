42186696dd48241f5122caf4e759aff2148a8a04
monitor <tracer@njuics.org>
2019-10-16 10:43:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/setcc.h
+++ nemu/include/cpu/instr/setcc.h
@@ -3,11 +3,11 @@
 
 make_instr_func(seto_b);
 make_instr_func(setno_b);
-make_instr_func(setb_b);
+make_instr_func(setc_b);
 make_instr_func(setae_b);
 make_instr_func(sete_b);
 make_instr_func(setne_b);
-make_instr_func(setna_b);
+make_instr_func(setbe_b);
 make_instr_func(seta_b);
 make_instr_func(sets_b);
 make_instr_func(setns_b);
