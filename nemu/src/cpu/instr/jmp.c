d5f0e15e051edc5b32d39478774cb4a4d70a1b78
tracer-pa-public <tracer@njuics.org>
2019-10-16 23:32:34 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:32:34 up 3 days, 21:41,  1 user,  load average: 0.07, 0.07, 0.10 3f3a3126ebe5dee7411b05c0c58f125118925c6e

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -18,6 +18,7 @@ make_instr_func(jmp_near)
 
         return 1 + data_size / 8;
 }
+
 make_instr_func(jmp_short)
 {
 	OPERAND rel;
@@ -37,4 +38,16 @@ make_instr_func(jmp_short)
         return 1 + 1;
 }
 
+make_instr_func(jmp_near_indirect)
+{
+	int len = 1;
+	OPERAND rel;
+        rel.data_size = data_size;
+	len += modrm_rm(eip + 1, &rel);
+        operand_read(&rel);
+        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
+        cpu.eip = rel.val;
+
+        return 0;
+}
