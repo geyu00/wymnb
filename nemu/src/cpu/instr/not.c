eb549214f6eb971f5da719b13709a9da1b3c591d
tracer-pa-public <tracer@njuics.org>
2019-10-16 21:57:59 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:57:59 up 3 days, 20:06,  1 user,  load average: 0.00, 0.02, 0.00 13b507c04466fcc21d486e068c92bb774ca3fb3e

---
+++ nemu/src/cpu/instr/not.c
@@ -0,0 +1,11 @@
+#include "cpu/instr.h"
+
+static void instr_execute_1op()
+{
+	operand_read(&opr_src);
+	opr_src.val = alu_xor(0xFFFFFFFF >> (32 - opr_src.data_size), opr_src.val; opr_src.data_size);
+	operand_write(&opr_src);
+}
+
+make_instr_impl_1op(not, rm, b)
+make_instr_impl_1op(not, rm, v)
