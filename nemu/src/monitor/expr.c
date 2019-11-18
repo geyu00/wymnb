7ade7f72e02b6e35bdb886dbc2432ddb3ec5f938
tracer-pa-public <tracer@njuics.org>
2019-11-18 11:54:50 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:54:50 up 3 days, 14:43,  1 user,  load average: 0.35, 0.23, 0.19 e4d98376e40364718467e9ea485a3f67fb3e4dd1

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -56,8 +56,8 @@ static struct rule
 	{"%", '%'},
 	{"\\(", '('},
 	{"\\)", ')'},
-	{"0[Xx][0-9a-fA-F]{1,8}", HEX,
-	{"[0-9]+", NUM}}
+	{"0[Xx][0-9a-fA-F]{1,8}", HEX},
+	{"[0-9]+", NUM}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]))
