eadf33f5075c5d84f6f227017e91b7edfa9df7fa
monitor <tracer@njuics.org>
2019-11-22 15:24:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,3 +1,17 @@
 #include "memory/cache.h"
 
 CacheLine L1_dcache[1024];
+
+void init_cache()
+{
+	
+}
+uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache);
+{
+	
+}
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine * cache)
+{
+	
+}
+
