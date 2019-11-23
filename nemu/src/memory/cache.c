0a88ea926370c3770d5d8562157145060c59829f
tracer-pa-public <tracer@njuics.org>
2019-11-23 17:34:07 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:34:07 up 3 days, 21:01,  1 user,  load average: 0.00, 0.00, 0.00 84077fec5f976b2f32aa6b4edca26de28c76e13

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,16 +1,17 @@
 #include "memory/cache.h"
 
-CacheLine L1_dcache[1024];
 
 void init_cache()
 {
-	
+	for (int i = 0; i < 128; i++)
+		for (int j = 0; j< 8; j++)
+			L1_dcache[i][j].valid = 0;
 }
-uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache);
+uint32_t cache_read(paddr_t paddr, size_t len, CacheLine cache[][8]);
 {
 	
 }
-void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine * cache)
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine cache[][8])
 {
 	
 }
