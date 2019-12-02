cd40001186b6b492889bdbcbd99f17ad6994346e
tracer-pa-public <tracer@njuics.org>
2019-12-02 17:05:59 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:05:59 up  4:11,  1 user,  load average: 0.20, 0.09, 0.02 f2021e5de8244dd2468c9f052ef87a09969bbab5

--- nemu/src/cpu/instr/ljmp.c
+++ nemu/src/cpu/instr/ljmp.c
@@ -3,14 +3,14 @@
 make_instr_func(ljmp)
 {
 	OPERAND res1, res2;
-	res1.type = OPR.IMM;
+	res1.type = OPR_IMM;
 	res1.sreg = SREG_CS;
 	res1.addr = eip + 1;
 	res1.data_size = data_size;
 	operand_read(&res1);
 	cpu.eip = res1.val;
 
-	res2.type = OPR.IMM;
+	res2.type = OPR_IMM;
 	res2.sreg = SREG_CS;
 	res2.addr = eip + 1;
 	res2.data_size = 16;
