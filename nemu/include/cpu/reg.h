0756c819847fe033ff6598c647f59c79f9bcef62
monitor <tracer@njuics.org>
2019-12-02 12:39:14 +0800

    git monitor commit.

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -85,6 +85,18 @@ typedef struct
 			SegReg es, cs, ss, ds, fs, gs;
 		};
 	};
+	typedef union {
+		struct {
+			uint32_t pe :1;
+			uint32_t mp :1;
+uint32_t em :1;
+uint32_t ts :1;
+uint32_t et :1;
+uint32_t reserve :26;
+uint32_t pg :1;
+};
+uint32_t val; 
+}CR0;
 	// control registers, todo: define type CR0
 	CR0 cr0;
 #else
