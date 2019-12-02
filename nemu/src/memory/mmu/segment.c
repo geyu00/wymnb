a46c295a1ad47a8d11c7f6df21039d2523ab9c36
tracer-pa-public <tracer@njuics.org>
2019-12-02 17:05:12 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:05:12 up  4:10,  1 user,  load average: 0.09, 0.07, 0.01 d1831ddf575f7b2b9f98d423fb36f28e532ef850

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -16,4 +16,13 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
+	uint32_t addr = cpu.gdtr.base + 8 * cpu.segReg[sreg].index;
+	SegDesc seg;
+	seg.val[0] = laddr_read(addr, 4);
+	seg.val[1] = laddr_read(addr + 4, 4);
+	cpu.segReg[sreg].base = seg.base_15_0 | (seg.base_23_16 << 16) | (seg.base_31_24 << 24);
+	cpu.segReg[sreg].limit = seg.limit_15_0 | (seg.limit_19_16 << 16);
+	cpu.segReg[sreg].type = seg.type;
+	cpu.segReg[sreg].privilege_level = seg.privilege_level;
+	cpu.segReg[sreg].soft_use = seg.soft_use;
 }
