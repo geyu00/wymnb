939e1d07009a284ab2b574fbda89b71142224c1b
tracer-pa-public <tracer@njuics.org>
2019-12-10 00:10:00 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  00:10:00 up 6 min,  1 user,  load average: 0.03, 0.07, 0.03 6fa8d125757eb81e84af2806684c0f82b85ae28f

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -26,7 +26,7 @@ void init()
 	 * is located at 0xc0030000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
 	init_page();
-
+	
 	/* After paging is enabled, transform %esp to virtual address. */
 	asm volatile("addl %0, %%esp"
 				 :
@@ -36,6 +36,7 @@ void init()
 /* Jump to init_cond() to continue initialization. */
 // need to plus the offset 0xc0000000 if using gcc-6, strange
 #ifdef IA32_PAGE
+	BREAK_POINT;
 	asm volatile("jmp *%0"
 				 :
 				 : "r"(init_cond + 0xc0000000));
