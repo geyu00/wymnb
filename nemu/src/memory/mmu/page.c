34a7f25acfbc7a8c52e51c640de94929cb8cb611
tracer-pa-public <tracer@njuics.org>
2019-12-09 22:08:35 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:08:35 up  5:59,  1 user,  load average: 0.10, 0.06, 0.02 f7cff06b11268764f5cd7ea471a179f85826f6fd

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -10,9 +10,9 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t dir = laddr >> 22;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
-	uint32_t mulu = (dir <<2) + (cpu.cr3.addr<< 12);
-	uint32_t base = (paddr_read(mulu,4) & 0xfffff000) + (page << 2);
-	return ((paddr_read( base , 4 ) & 0xfffff000) + offset);
+	uint32_t mulu = (dir <<2 ) + (cpu.cr3.pdbr << 12);
+	uint32_t base = (paddr_read(mulu, 4) & 0xfffff000) + (page << 2);
+	return (paddr_read(base, 4) & 0xfffff000) | offset;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 	;
