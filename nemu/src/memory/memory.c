a44fc30027fe0b17223b880a49dd3a120df4d678
monitor <tracer@njuics.org>
2019-12-10 01:29:01 +0800

    git monitor commit.

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -45,7 +45,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 	//printf("!  %d    !",cpu.cr0.val);
 	//printf("!  %d    !",cpu.cr3.val);
 	assert(len == 1 || len == 2 || len == 4);//printf("@!#!");	
-	if (cpu.cr0.pg == 1)
+	if (cpu.cr0.pg == 1 && cpu.cr0.pe == 1)
 	{
 		if ((laddr & 0xfff) + len > 0x1000)
 		{
@@ -65,7 +65,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
-	if (cpu.cr0.pg == 1)
+	if (cpu.cr0.pg == 1 && cpu.cr0.pe == 1)
 	{
 		if ((laddr & 0xfff) + len > 0x1000)
 		{
