71f60108116577478c0ac9e6b8c962c2ddf4ec58
monitor <tracer@njuics.org>
2019-12-09 23:42:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -14,21 +14,21 @@ static void instr_execute_1op()
 	operand_write(&opr_dest);*/
 	
 
-	/*operand_read(&opr_src);
+	operand_read(&opr_src);
 	//cpu.esp -= opr_src.data_size / 8;
 	cpu.esp -= data_size / 8;
 	opr_src.data_size = data_size;
 	opr_src.addr = cpu.esp;	
 	opr_src.type = OPR_MEM;
-	operand_write(&opr_src);*/
+	operand_write(&opr_src);
 
-	cpu.esp -= data_size / 8;
+	/*cpu.esp -= data_size / 8;
 	opr_dest.data_size = data_size;
 	opr_dest.addr = cpu.esp;
 	opr_dest.type = OPR_MEM;
 	operand_read(&opr_src);
 	opr_dest.val = opr_src.val;
-	operand_write(&opr_dest);
+	operand_write(&opr_dest);*/
 }
 make_instr_impl_1op(push, r, v)
 make_instr_impl_1op(push, rm, v)
