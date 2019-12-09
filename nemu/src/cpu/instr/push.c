c0637ab6054bbb5736338373588f657a5dfbaca9
tracer-pa-public <tracer@njuics.org>
2019-12-09 23:40:52 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:40:52 up  7:32,  1 user,  load average: 0.08, 0.09, 0.04 ee0a7915ed804359d3c60f2373f349162736bd3

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -25,7 +25,7 @@ static void instr_execute_1op()
 	cpu.esp -= data_size / 8;
 	opr_dest.data_size = data_size;
 	opr_dest.addr = cpu.esp;
-	opr_dest.type = OPR_MEN;
+	opr_dest.type = OPR_MEM;
 	operand_read(&opr_src);
 	opr_dest.val = opr_src.val;
 	operand_write(&opr_dest);
