e5b31a7ba01d2c047fb58cb4a73e0bfd21f23f00
tracer-pa-public <tracer@njuics.org>
2019-12-10 10:49:11 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  10:49:11 up  9:56,  1 user,  load average: 0.82, 0.32, 0.12 f8223031ad07d5b2ab18b4ce78061b0819c1a2e0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -67,7 +67,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
-	if (cpu.cr0.pg == 1 && cpu.cr0.pe == 1)
+	if (cpu.cr0.pg == 1 /*&& cpu.cr0.pe == 1*/)
 	{
 		/*if ((laddr & 0xfff) + len > 0x1000)
 		{
