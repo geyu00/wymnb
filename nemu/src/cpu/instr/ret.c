1af5f12f8b7e1154ab11f3cde11da14decf13fc5
tracer-pa-public <tracer@njuics.org>
2019-12-24 22:59:38 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:59:38 up 1 day, 11:48,  1 user,  load average: 0.03, 0.04, 0.00 fca19c332da91d5834ad47277abbf814e0e8dc4f

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -46,3 +46,62 @@ make_instr_func(ret_near_imm16)
 	cpu.esp += imm_val;
 	return 0;
 }
+
+make_instr_func(iret)
+{
+	//pop eip
+	OPERAND dest1;
+	dest1.data_size = 32;
+	dest1.type = OPR_MEM;
+	dest1.sreg = SREG_SS;
+	dest1.addr = cpu.esp;
+	operand_write(&dest1);
+	cpu.esp += 4;
+	cpu.eip = dest1.val;
+
+	//pop cs
+	OPERAND dest2;
+	dest2.data_size = 32;
+	dest2.type = OPR_MEM;
+	dest2.sreg = SREG_SS;
+	dest2.addr = cpu.esp;
+	operand_write(&dest2);
+	cpu.esp += 4;
+	cpu.cs.val = dest2.val;
+
+	//pop eflags
+	OPERAND dest3;
+	dest3.data_size = 32;
+	dest3.type = OPR_MEM;
+	dest3.sreg = SREG_SS;
+	dest3.addr = cpu.esp;
+	operand_write(&dest3);
+	cpu.esp += 4;
+	cpu.eflags.val = dest3.val;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
