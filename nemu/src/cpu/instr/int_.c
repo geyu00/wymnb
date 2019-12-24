2fb393a46a5b08409b94e93d805cab0fb160f7dc
monitor <tracer@njuics.org>
2019-12-24 20:48:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/int_.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(int_)
+{
+	OPERAND imm;
+	imm.data_size = 8;
+	imm.type = OPR_IMM;
+	imm.sreg = SREG_CS;
+	imm.addr = eip + 1;
+	operand_read(&imm);
+	raise_sw_intr(imm.val);
+	print_asm_1("int" ,"", 1, &imm);
+
+	return 0;	
+}
