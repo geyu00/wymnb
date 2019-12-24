962221b695354dab9c65dc94e0984872b7fc12e9
tracer-pa-public <tracer@njuics.org>
2019-12-24 22:19:59 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:19:59 up 1 day, 11:09,  1 user,  load average: 0.07, 0.03, 0.01 d917df018198a3495de31708e1be143efe103193

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -39,7 +39,7 @@ void raise_intr(uint8_t intr_no)
 	laddr_t addr = cpu.idtr.base + 8 * intr_no;
 	gatedesc.val[0] = laddr_read(addr, 4);
 	gatedesc.val[1] = laddr_read(addr + 4, 4);
-	//assert(gatedesc.present == 1);
+	assert(gatedesc.present == 1);
 	cpu.cs.val = gatedesc.selector;
 	load_sreg(SREG_CS);
 	if (gatedesc.type == 0xe)
