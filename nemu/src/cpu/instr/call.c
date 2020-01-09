d1e467b53e449298743d67651b6ce3395eb3e2f8
tracer-pa-public <tracer@njuics.org>
2020-01-09 19:23:39 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:23:39 up 19:12,  1 user,  load average: 0.08, 0.08, 0.08 8e1aeb3cd470622bb391309af3ce62cb9e12b890

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -44,7 +44,7 @@ make_instr_func(call_near_indirect)
 	tem.data_size = data_size;
 	tem.addr = cpu.esp;
 	//tem.val = eip + 1 + data_size / 8;
-	tem.val = eip;
+	tem.val = eip + len;
 	operand_write(&tem);
 
         print_asm_1("call", "", 1 + data_size / 8, &rel);
