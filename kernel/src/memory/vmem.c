6afb401e2b7d663d3ef316a3a68e09d0baf6564d
tracer-pa-public <tracer@njuics.org>
2020-01-16 16:00:52 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:00:52 up  4:55,  1 user,  load average: 0.90, 0.77, 0.48 51d1a85f2fc3cb062ec79359e092e75e8a4ac2f4

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -27,7 +27,7 @@ void create_video_mapping()
 	PTE *ptable = (PTE*)va_to_pa(vptable);
 	uint32_t pdir_idx, ptable_idx, pframe_idx;
 	pframe_idx = 0;
-	for(pdir_idx = 0; pdir_idx < 2; pdir_idx++)
+	for(pdir_idx = 0; pdir_idx < 1; pdir_idx++)
 	{
 		//if(pframe_idx > 0xaf)break;
 		pdir[pdir_idx].val = make_pde(ptable);
