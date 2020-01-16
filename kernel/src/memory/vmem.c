c6845c52ad81b47e4d5a69666a8ae94d71c38512
monitor <tracer@njuics.org>
2020-01-16 15:18:01 +0800

    git monitor commit.

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -6,7 +6,8 @@
 #define SCR_SIZE (320 * 200)
 #define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) // number of page tables to cover the vmem
 
-PTE vptable[NR_PTE] align_to_page; // kernel page tables
+//PDE vpdir[NR_PDE] align_to_page;
+PTE vptable[NR_PT] align_to_page; // kernel page tables
 
 PDE *get_updir();
 
@@ -18,7 +19,31 @@ void create_video_mapping()
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	//panic("please implement me");
+
+
+
+	PDE *pdir = get_updir();
+	PTE *ptable = (PTE*)va_to_pa(vptable);
+	uint32_t pdir_idx, ptable_idx, pframe_idx;
+	pframe_idx = 0;
+	for(pdir_idx = 0; pdir_idx < 2; pdir_idx ++){
+		if(pframe_idx > 0xaf){break;}
+		pdir[pdir_idx].val = make_pde(ptable);
+		for(ptable_idx = 0; ptable_idx < NR_PTE; ptable_idx++){
+			if(pframe_idx > 0xaf){
+				break;
+			}
+			if(pframe_idx < 0xa0){
+				ptable->val = 0;
+			}
+			else{
+				ptable->val = make_pte(pframe_idx << 12);
+			}
+			pframe_idx++;
+			ptable ++;
+		}
+	}
 }
 
 void video_mapping_write_test()
