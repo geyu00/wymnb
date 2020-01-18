3512de65bcb08bdb1d6514ecdb794eb50b279459
monitor <tracer@njuics.org>
2020-01-18 15:40:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/neg.c
+++ nemu/src/cpu/instr/neg.c
@@ -5,7 +5,7 @@ make_instr_func(neg_rm_v)
 	int len = 1;
 	OPERAND rm;
 	rm.data_size = data_size;
-	len += modrm_rm(eip + 1, &rm);
+	len += modrm_rm(cpu.eip + 1, &rm);
 	operand_read(&rm);
 	if (rm.val == 0)
 		cpu.eflags.CF = 0;
