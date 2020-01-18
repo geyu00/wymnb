e3e45d3af43b497edf9dced1c9b1154100b9f6d4
tracer-pa-public <tracer@njuics.org>
2020-01-18 15:32:19 +0800

    >  nemu 181860022 geyu Linux gy 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:32:19 up  1:05,  1 user,  load average: 0.10, 0.10, 0.11 920bec0a1a3c76b86b522ea010a238c0e7718e8e

--- nemu/src/cpu/instr/neg.c
+++ nemu/src/cpu/instr/neg.c
@@ -2,5 +2,17 @@
 
 make_instr_func(neg_rm_v)
 {
-
+	int len = 1;
+	OPERAND rm;
+	rm.data_size = data_size;
+	len += modrm_rm(eip + 1, &rm);
+	operand_read(&rm);
+	if (rm.val == 0)
+		cpu.eflags.CF = 0;
+	else
+		cpu.eflags.CF = 1;
+	rm.val = -rm.val;
+	operand_write(&rm);
+	print_asm_1("neg", "", len, &rm);
+	return len;
 }
